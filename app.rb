2e4e3bc826cc004cb7262a363d7478d743c2a22a
伊藤 大智 <daichi.ito@ever-rise.co.jp>
2018-03-17 21:04:07 +0900

    •コミット誤りのため修正•アクセス情報再生成

--- app.rb
+++ app.rb
@@ -24,14 +24,11 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider:              'AWS',
-    # aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-    aws_access_key_id:     'AKIAJXIAD4C3P3DQRY4Q',
-    # aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    aws_secret_access_key: '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',
+    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
     region:                'ap-northeast-1'
   }
-  # config.fog_directory  = ENV['S3_BUCKET_NAME']
-  config.fog_directory  = 'bookshelf-image'
+  config.fog_directory  = ENV['S3_BUCKET_NAME']
   config.fog_public = true
 end
 
