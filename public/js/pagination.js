abd469d9b1ce6dad6fe5b9a38578218e6cf12907
伊藤 大智 <daichi.ito@ever-rise.co.jp>
2018-04-15 14:29:26 +0900

    インデント修正

--- public/js/pagination.js
+++ public/js/pagination.js
@@ -5,12 +5,12 @@ function setPagination(status, records) {
     pages = Math.ceil(records / PER_PAGE_LIMIT);
     if (records > PER_PAGE_LIMIT) {
         $('#paging').pagination({
-            items: pages,
-            itemOnPage: PER_PAGE_LIMIT,
+            items      : pages,
+            itemOnPage : PER_PAGE_LIMIT,
             currentPage: currentPage,
-            cssStyle: 'dark-theme',
-            prevText: '<span aria-hidden="true">&laquo;</span>',
-            nextText: '<span aria-hidden="true">&raquo;</span>',
+            cssStyle   : 'dark-theme',
+            prevText   : '<span aria-hidden="true">&laquo;</span>',
+            nextText   : '<span aria-hidden="true">&raquo;</span>',
             onPageClick: function (page, evt) {
                 showTargetBooks(status, page);
             }
