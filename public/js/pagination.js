e9f0aaf616411abd4ce0ec63f0f6317b015ef034
伊藤 大智 <daichi.ito@ever-rise.co.jp>
2018-04-21 22:09:47 +0900

    ページネートが表示されない問題を修正

--- public/js/pagination.js
+++ public/js/pagination.js
@@ -2,19 +2,20 @@ const PER_PAGE_LIMIT = 20;
 var currentPage;
 
 function setPagination(status, records) {
+    $('#paging').pagination({
+        items      : pages,
+        itemOnPage : PER_PAGE_LIMIT,
+        currentPage: currentPage,
+        cssStyle   : 'dark-theme',
+        prevText   : '<span aria-hidden="true">&laquo;</span>',
+        nextText   : '<span aria-hidden="true">&raquo;</span>',
+        onPageClick: function (page, evt) {
+            showTargetBooks(status, page);
+        }
+    });
     pages = Math.ceil(records / PER_PAGE_LIMIT);
     if (records > PER_PAGE_LIMIT) {
-        $('#paging').pagination({
-            items      : pages,
-            itemOnPage : PER_PAGE_LIMIT,
-            currentPage: currentPage,
-            cssStyle   : 'dark-theme',
-            prevText   : '<span aria-hidden="true">&laquo;</span>',
-            nextText   : '<span aria-hidden="true">&raquo;</span>',
-            onPageClick: function (page, evt) {
-                showTargetBooks(status, page);
-            }
-        });
+        $('#paging').show();
     } else {
         $('#paging').hide();
     }
